006be36b630c5cc2e4c60a233d55cc8ffcd71d68
José Matías Rivero <matias@ensolvers.com>
2021-07-20 14:32:33 -0300

    First implementation of AutomaticLogging annotation

---
+++ modules/fox-spring/src/main/java/com/ensolvers/fox/spring/aop/AutomaticLoggingImpl.java
@@ -0,0 +1,102 @@
+package com.ensolvers.fox.spring.aop;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+
+/**
+ * AutomaticLogging default implementation
+ */
+@Aspect
+@Configuration
+public class AutomaticLoggingImpl {
+
+  public static <T> boolean switchType(Object o, Consumer... a) {
+    for (Consumer consumer : a)
+      if (o instanceof Collection || o instanceof Map) {
+        consumer.accept(o);
+        return true;
+      }
+      return false;
+  }
+
+  public static <T> Consumer caze(Class<T> cls, Consumer<T> c) {
+    return obj -> Optional.of(obj).filter(cls::isInstance).map(cls::cast).ifPresent(c);
+  }
+
+  @Around("@annotation(com.ensolvers.fox.spring.aop.AutomaticLogging)")
+  public Object logExecution(ProceedingJoinPoint joinPoint) throws Throwable {
+    long start = System.currentTimeMillis();
+
+    // The names and values of the target parameters are captured, as well as the values in the annotation.
+    MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+    Logger logger = LoggerFactory.getLogger(AutomaticLogging.class);
+    AutomaticLogging annotation = methodSignature.getMethod().getAnnotation(AutomaticLogging.class);
+    String className = joinPoint.getTarget().getClass().getName();
+
+    AtomicReference<String> logContent = new AtomicReference<String>(
+            (!annotation.logSuffix().equals("") ?
+                          "[" + annotation.logSuffix() + "] ": "") +
+                        "["+ className + "] " + "[" + methodSignature.getName() + "] " +
+                        (annotation.timeElapsedLogging() ? "[Call] " : ""));
+    String[] parameterNames = methodSignature.getParameterNames();
+    Object[] parameterValues = joinPoint.getArgs();
+
+    // Iterate between the parameters if the option has not been disabled in the annotation and add them to the String
+    if (annotation.includeParameters()) {
+      addParametersToLog(annotation, logContent, parameterNames, parameterValues);
+    }
+
+    logger.info(logContent.get());
+
+    // We proceed with the execution of the target
+    Object proceed = null;
+    try {
+      proceed = joinPoint.proceed();
+
+      // When it is finished, we calculate how long it took and the message is updated to be logged again if TimeElapsedLogging is activated */
+      if (annotation.timeElapsedLogging()){
+        long executionTime = System.currentTimeMillis() - start;
+        logContent.set(logContent.get().replace("[Call]", "[Finish]"));
+        logContent.set(logContent.get() + "elapsed ms: " + executionTime + " (" + TimeUnit.MILLISECONDS.toSeconds(executionTime) + " sec)");
+        logger.info(logContent.get());
+      }
+    } catch(Exception error){
+      logger.error((!annotation.logSuffix().equals("") ? "[" + annotation.logSuffix() + "] ": "") + "["+ className + "] " + "[" + methodSignature.getName() + "] Error: " + error);
+      throw error;
+    }
+
+    return proceed;
+  }
+
+  private void addParametersToLog(AutomaticLogging annotation, AtomicReference<String> logContent, String[] parameterNames, Object[] parameterValues) {
+    if (annotation.logCollectionsSizeOnly()) {
+      // If the parameters are Collections and the logCollectionsSizeOnly() parameter of the Annotation is set to True, only the size will be reported and not the content.
+      for (int i = 0; i < parameterNames.length; i++) {
+        logContent.set(logContent.get() + parameterNames[i]);
+        boolean collectionParam = switchType(parameterValues[i],
+            caze(Collection.class, c -> logContent.set(logContent.get() + " size : " + c.size() + " ")),
+            caze(Map.class, a -> logContent.set(logContent.get() + " size : " + a.size() + " "))
+          );
+        if (!collectionParam){
+          logContent.set(logContent.get() + ": " + (parameterValues[i] != null ? parameterValues[i].toString() : "null") + " ");
+        }
+      }
+    } else {
+      for (int i = 0; i < parameterNames.length; i++) {
+        logContent.set(logContent.get() + parameterNames[i] + ": " + parameterValues[i].toString() + " ");
+      }
+    }
+  }
+}
+
