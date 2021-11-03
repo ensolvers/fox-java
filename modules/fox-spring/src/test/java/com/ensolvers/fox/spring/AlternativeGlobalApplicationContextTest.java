bbcb955943e8daa3ee42420e5b1e141d7d9ae251
emilioOcaEns <emilio.o@ensolvers.com>
2021-11-02 22:22:31 -0300

    indexable Spring application context

---
+++ modules/fox-spring/src/test/java/com/ensolvers/fox/spring/AlternativeGlobalApplicationContextTest.java
@@ -0,0 +1,38 @@
+package com.ensolvers.fox.spring;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import com.ensolvers.fox.services.logging.Logger;
+import com.ensolvers.fox.spring.lightweightcontainer.GlobalApplicationContext;
+import java.io.InputStream;
+import java.util.Collection;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+public class AlternativeGlobalApplicationContextTest {
+
+   @Test
+   public void testResourceSample() {
+      GlobalApplicationContext.build( "classpath*:/com/ensolvers/fox/spring/sameAutowiring-context.xml");
+      SampleComponent sc = (SampleComponent)GlobalApplicationContext.getBean("sampleComponent" );
+      assertNotNull( sc );
+      assertEquals( "Hey there", sc.helloWorld() );
+
+   }
+
+   @Test
+   public void testPointcutResourceSample() {
+      GlobalApplicationContext.build( "classpath*:/com/ensolvers/fox/spring/pointcutAutowiring-context.xml");
+
+      try {
+         GlobalApplicationContext.getBean("sampleComponent");
+         fail();
+      } catch (Exception e) {}
+
+      assertNotNull(GlobalApplicationContext.getBean("automaticLoggingImpl" ) );
+   }
+
+}
