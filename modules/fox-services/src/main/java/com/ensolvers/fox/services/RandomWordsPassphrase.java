eae4efce36ee25acaf31f0d7141f58791dac34c5
Agustin Del Torto <agustindeltorto@gmail.com>
2021-12-15 15:25:12 -0300

    adopt singleton pattern, add logger

--- modules/fox-services/src/main/java/com/ensolvers/fox/services/RandomWordsPassphrase.java
+++ modules/fox-services/src/main/java/com/ensolvers/fox/services/RandomWordsPassphrase.java
@@ -1,18 +1,38 @@
 package com.ensolvers.fox.services;
 
-import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.time.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class RandomWordsPassphrase {
+public final class RandomWordsPassphrase {
+
+    private static RandomWordsPassphrase instance;
+    private final List<String> dictionary;
+    private static Logger logger = LoggerFactory.getLogger(RandomWordsPassphrase.class);
+
+    private RandomWordsPassphrase() throws IOException {
+        // Load english dictionary to memory
+        StopWatch stopWatch = StopWatch.createStarted();
+        File dictionaryFile = new File("src/main/resources/words_alpha.txt");
+        try (Stream<String> dictionary = Files.lines(dictionaryFile.toPath())) {
+            this.dictionary = dictionary.collect(Collectors.toList());
+            stopWatch.stop();
+            logger.info("Dictionary was loaded to memory in {}ms", stopWatch.getTime(TimeUnit.MILLISECONDS));
+        }
+    }
 
     private static <T> T pickRandomItemFromList(List<T> originalList) {
-        int idx = (int)(Math.random() * originalList.size());
+        Random r = new Random();
+        int idx = r.nextInt(originalList.size() - 1);
         T randomItem = null;
         if (!originalList.isEmpty()) {
             randomItem = originalList.get(idx);
@@ -20,35 +40,30 @@ public class RandomWordsPassphrase {
         return randomItem;
     }
 
-    private static String randomWords(int n)  {
-        try {
-            // Load english dictionary to memory
-            File dictionaryFile = new File("src/main/resources/words_alpha.txt");
-            Stream<String> dictionary = Files.lines(dictionaryFile.toPath());
-            List<String> wordList = dictionary.collect(Collectors.toList());
-            StringBuilder words = new StringBuilder();
-
-            int i = 0;
-            while (i < n) {
-                String currentWord = pickRandomItemFromList(wordList);
-                if (currentWord.length() > 3 && currentWord.length() < 8) {
-                    words.append(currentWord);
-                    if (i < n - 1) words.append(" ");
-                    ++i;
-                }
+    private String randomWords(int n)  {
+        StringBuilder words = new StringBuilder();
+        int i = 0;
+        while (i < n) {
+            String currentWord = pickRandomItemFromList(dictionary);
+            if (currentWord.length() > 3 && currentWord.length() < 8) {
+                words.append(currentWord);
+                if (i < n - 1) words.append(" ");
+                ++i;
             }
-
-            return words.toString();
-        } catch (IOException e) {
-            return RandomStringUtils.randomAlphanumeric(18);
         }
+        return words.toString();
     }
 
-    public static String generate() {
+    public static RandomWordsPassphrase getInstance() throws IOException {
+        if (instance == null) {
+            instance = new RandomWordsPassphrase();
+        }
+        return instance;
+    }
+    public String generate() {
         return randomWords(4);
     }
-
-    public static String generate(int n) {
+    public String generate(int n) {
         return randomWords(n);
     }
 }
--- modules/fox-services/src/test/java/com/ensolvers/fox/services/RandomWordsPassphraseTest.java
+++ modules/fox-services/src/test/java/com/ensolvers/fox/services/RandomWordsPassphraseTest.java
@@ -3,6 +3,7 @@ package com.ensolvers.fox.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,8 +11,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class RandomWordsPassphraseTest {
 
     @Test
-    void generationTest() {
-        String passphrase = RandomWordsPassphrase.generate();
+    void generationTest() throws IOException {
+        RandomWordsPassphrase generator = RandomWordsPassphrase.getInstance();
+        String passphrase = generator.generate();
         String[] words = passphrase.split(" ");
         Assertions.assertEquals(4, words.length);
         for (String word: words) {
